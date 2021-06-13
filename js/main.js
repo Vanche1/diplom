7b696203317165bd77aba5316dea27108b3d31c0
Vanche1 <81378831+Vanche1@users.noreply.github.com>
2021-06-13 11:23:27 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+
+	$('.menu__btn').on('click', function() {
+		$('.menu__btn').toggleClass('menu__btn--active');
+		$('.menu__list').toggleClass('menu__list--active');
+		$('.submenu__list').toggleClass('submenu__list--active');
+		$('.submenu').toggleClass('submenu--active');
+		$('.header').toggleClass('header--active');
+	});
+
+	$('.menu__list-link').on('click', function() {
+		$('.menu__btn').removeClass('menu__btn--active');
+		$('.menu__list').removeClass('menu__list--active');
+		$('.submenu__list').removeClass('submenu__list--active');
+		$('.submenu').removeClass('submenu--active');
+		$('.header').removeClass('header--active');
+	});
+
+});
